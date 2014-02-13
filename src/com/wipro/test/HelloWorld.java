6242ed610562d70e3948ebd2f4dab91cb02a8218
katyaap <kathir1975@yahoo.com>
2014-02-13 11:12:55 +0530

    added Helloworld comments

--- src/com/wipro/test/HelloWorld.java
+++ src/com/wipro/test/HelloWorld.java
@@ -3,6 +3,6 @@ package com.wipro.test;
 public class HelloWorld {
 
 	public static void main(String[] args){
-System.out.println("Hello world!!");		
+System.out.println("Welcome to Hello world!!- automated java program");	
 	}
 }
